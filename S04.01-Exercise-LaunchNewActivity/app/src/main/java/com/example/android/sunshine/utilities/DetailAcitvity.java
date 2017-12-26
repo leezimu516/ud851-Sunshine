636b1befeb0d5bfa48da46a3256d268c47754caf
kai li <adamlee516@gmail.com>
2017-12-26 10:10:20 -0500

    launch new activity

---
+++ S04.01-Exercise-LaunchNewActivity/app/src/main/java/com/example/android/sunshine/utilities/DetailAcitvity.java
@@ -0,0 +1,19 @@
+package com.example.android.sunshine.utilities;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.android.sunshine.R;
+
+/**
+ * Created by kaili on 2017-12-26.
+ */
+
+public class DetailAcitvity extends AppCompatActivity{
+	@Override
+	protected void onCreate(@Nullable Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_detail);
+	}
+}
